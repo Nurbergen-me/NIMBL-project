b387626c5acd2a033b1da8c96b029a8fd337d77b
Nurbergen-me <53029070+Nurbergen-me@users.noreply.github.com>
2022-11-23 10:12:45 +0600

    last updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NIMBL-project/',
+  base: '/web/nimbl/',
   plugins: [vue()],
   resolve: {
     alias: {
